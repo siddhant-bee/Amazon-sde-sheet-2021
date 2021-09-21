b8cd9f7d6c1ff654539011468fd75f5beb9706b2
siddhant <79956889+siddhant-bee@users.noreply.github.com>
2021-09-21 21:21:18 +0530

    Update 04 minDist2nos.cpp

--- 01.Array/04 minDist2nos.cpp
+++ 01.Array/04 minDist2nos.cpp
@@ -1,19 +1,25 @@
 
 int minDist(int a[], int n, int x, int y) {
+        int recent_x = -1, recent_y = -1;
+        int ans = INT_MAX;
     
-    int ans = INT_MAX;
-    int prev = -1;
+        // Storing the recent x or y and calculating the minimum distance
+        for (int i = 0; i < n; i++) {
+            if (a[i] == x) {
+                recent_x = i;
     
-    for (int i = 0; i < n; ++i){
-        if (a[i] == x || a[i] == y){
-            if (prev != -1 && a[prev] != a[i])
-                ans = min(ans, i-prev);
-            prev = i;
+                // Checking both recent X and Y is found in the array or not
+                if (recent_y != -1) ans = min(abs(recent_x - recent_y), ans);
+            } else if (a[i] == y) {
+                recent_y = i;
+    
+                // Checking both recent X and Y is found in the array or not
+                if (recent_x != -1) ans = min(abs(recent_x - recent_y), ans);
+            }
         }
-    }
     
-    if (ans == INT_MAX)
-        return -1;
-        
-    return ans;
-}
+        // If ans is not updated then either x or y, or both are not present in the
+        // array
+        if (ans == INT_MAX) return -1;
+        return ans;
+    }
